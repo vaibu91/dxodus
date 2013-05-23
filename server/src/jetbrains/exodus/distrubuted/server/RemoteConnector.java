c23d9c3bb5030bec217c4c2d04f7321157ea4248
pavel.nikitin <pavel.nikitin@jetbrains.com>
2013-05-23 18:44:16 +0400

    use futures and re-use client

--- server/src/jetbrains/exodus/distrubuted/server/RemoteConnector.java
+++ server/src/jetbrains/exodus/distrubuted/server/RemoteConnector.java
@@ -1,9 +1,6 @@
 package jetbrains.exodus.distrubuted.server;
 
-import com.sun.jersey.api.client.Client;
-import com.sun.jersey.api.client.ClientResponse;
-import com.sun.jersey.api.client.GenericType;
-import com.sun.jersey.api.client.WebResource;
+import com.sun.jersey.api.client.*;
 import com.sun.jersey.api.client.config.ClientConfig;
 import com.sun.jersey.api.client.config.DefaultClientConfig;
 import com.sun.jersey.api.json.JSONConfiguration;
@@ -14,6 +11,10 @@ import org.jetbrains.annotations.Nullable;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.MultivaluedMap;
 import java.util.Arrays;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 public class RemoteConnector {
 
@@ -22,14 +23,31 @@ public class RemoteConnector {
     private static final GenericType<String[]> STRING_ARR_TYPE = new GenericType<>(String[].class);
     private static final GenericType<ClientResponse> RESP_TYPE = new GenericType<>(ClientResponse.class);
 
-    public String get(@NotNull final String url, @NotNull final String ns, @NotNull final String key) {
-        return get(url, ns, key, null);
+    private final Client c;
+
+    public RemoteConnector() {
+        ClientConfig clientConfig = new DefaultClientConfig();
+        clientConfig.getFeatures().put(JSONConfiguration.FEATURE_POJO_MAPPING, Boolean.TRUE);
+        clientConfig.getProperties().put(ClientConfig.PROPERTY_FOLLOW_REDIRECTS, Boolean.TRUE);
+        c = Client.create(clientConfig);
+    }
+
+    public String get(@NotNull final String url, @NotNull final String ns, @NotNull final String key, long timeout) throws TimeoutException {
+        return get(url, ns, key, timeout, null);
     }
 
     public String get(@NotNull final String url, @NotNull final String ns,
-                      @NotNull final String key, @Nullable final Long timeStamp) {
-        Client c = createClient();
-        WebResource r = c.resource(url + ns + '/' + key);
+                      @NotNull final String key, long timeout, @Nullable final Long timeStamp) throws TimeoutException {
+        return wrapFuture(timeout, getAsync(url, ns, key, timeStamp));
+    }
+
+    public Future<String> getAsync(@NotNull final String url, @NotNull final String ns, @NotNull final String key) {
+        return getAsync(url, ns, key, null);
+    }
+
+    public Future<String> getAsync(@NotNull final String url, @NotNull final String ns,
+                                   @NotNull final String key, @Nullable final Long timeStamp) {
+        AsyncWebResource r = c.asyncResource(url + ns + '/' + key);
         if (timeStamp != null) {
             r = r.queryParam("timeStamp", timeStamp.toString());
         }
@@ -37,14 +55,25 @@ public class RemoteConnector {
     }
 
     public ClientResponse put(@NotNull final String url, @NotNull final String ns,
-                        @NotNull final String key, @NotNull String value) {
-        return put(url, ns, key, value, null);
+                              @NotNull final String key, long timeout, @NotNull String value) throws TimeoutException {
+        return put(url, ns, key, value, timeout, null);
     }
 
-    public ClientResponse put(@NotNull final String url, @NotNull final String ns,
-                      @NotNull final String key, @NotNull String value, @Nullable final Long timeStamp) {
-        Client c = createClient();
-        WebResource r = c.resource(url + ns + '/' + key);
+    public ClientResponse put(@NotNull final String url, @NotNull final String ns, @NotNull final String key,
+                              @NotNull String value, long timeout, @Nullable final Long timeStamp) throws TimeoutException {
+        return wrapFuture(timeout, putAsync(url, ns, key, value, timeStamp));
+    }
+
+    @NotNull
+    public Future<ClientResponse> putAsync(@NotNull final String url, @NotNull final String ns, @NotNull final String key,
+                                           @NotNull String value) {
+        return putAsync(url, ns, key, value, null);
+    }
+
+    @NotNull
+    public Future<ClientResponse> putAsync(@NotNull final String url, @NotNull final String ns, @NotNull final String key,
+                                           @NotNull String value, @Nullable final Long timeStamp) {
+        AsyncWebResource r = c.asyncResource(url + ns + '/' + key);
         if (timeStamp != null) {
             r = r.queryParam("timeStamp", timeStamp.toString());
         }
@@ -54,30 +83,37 @@ public class RemoteConnector {
         return r.post(RESP_TYPE, formData);
     }
 
-    public String[] friends(@NotNull final String url) {
-        Client c = createClient();
-        WebResource r = c.resource(url + "friends");
-        return r.get(STRING_ARR_TYPE);
+    public String[] friends(@NotNull final String url, long timeout) throws TimeoutException {
+        return wrapFuture(timeout, friendsAsync(url));
     }
 
-    private Client createClient() {
-        ClientConfig clientConfig = new DefaultClientConfig();
-        clientConfig.getFeatures().put(JSONConfiguration.FEATURE_POJO_MAPPING, Boolean.TRUE);
-        clientConfig.getProperties().put(ClientConfig.PROPERTY_FOLLOW_REDIRECTS, Boolean.TRUE);
-        return Client.create(clientConfig);
+    private Future<String[]> friendsAsync(String url) {
+        AsyncWebResource r = c.asyncResource(url + "friends");
+        return r.get(STRING_ARR_TYPE);
     }
 
     public static RemoteConnector getInstance() {
         return INSTANCE;
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws TimeoutException {
         // simple connector test
         final String val = Long.toBinaryString(System.currentTimeMillis());
         final String url = "http://localhost:8086/";
         final RemoteConnector conn = RemoteConnector.getInstance();
-        System.out.println(conn.put(url, "ns1", "key2", val).getStatus());
-        System.out.println(conn.get(url, "ns1", "key2"));
-        System.out.println(Arrays.toString(conn.friends(url)));
+        System.out.println(conn.put(url, "ns1", "key2", 1000, val).getStatus());
+        System.out.println(conn.get(url, "ns1", "key2", 1000));
+        System.out.println(Arrays.toString(conn.friends(url, 1000)));
+    }
+
+    private static <T> T wrapFuture(final long timeout, @NotNull final Future<T> future) throws TimeoutException {
+        try {
+            return future.get(timeout, TimeUnit.MILLISECONDS);
+        } catch (InterruptedException | ExecutionException e) {
+            throw new RuntimeException(e);
+        } catch (TimeoutException t) {
+            System.out.println(future.cancel(true) ? "Cancelled future" : "Timed out future");
+            throw t;
+        }
     }
 }
